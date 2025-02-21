db8ebd8114c9d41199019a15347b1d463e348dd6
indu chaurasia <indu.bitmesra@gmail.com>
2025-02-20 18:29:46 -0600

    AOC day 11 optimised

--- src/main/java/com/aoc/challange/year_2024/Day11.java
+++ src/main/java/com/aoc/challange/year_2024/Day11.java
@@ -9,95 +9,57 @@ import java.util.*;
  * @see <a href="https://adventofcode.com/2024/day/11/input">input</a>
  */
 public class Day11 {
-
-    private static class Node {
-        Long val;
-        Node next;
-
-        public Node(Long val) {
-            this.val = val;
-        }
-
-        public Node(String val) {
-            this.val = Long.parseLong(val);
-        }
-    }
-
-    private record StoneArrangement(Node head, Node tail, long totalNodes) {
-    }
-
-    private StoneArrangement readInput() {
+    private Map<String, Long> cache = new HashMap<>();
+    private int[] readInput() {
         List<String> l = InputFormatter.formatLines("input/2024/Day11").stream().toList();
-        Node head = null;
-        Node previous = null;
-        for (String s : l.get(0).split(" ")) {
-            Node n = new Node(Long.parseLong(s));
-            if (head == null) {
-                head = n;
-            } else {
-                previous.next = n;
-            }
-            previous = n;
-        }
-        return new StoneArrangement(head, previous, l.get(0).split(" ").length);
+        return Arrays.stream(l.get(0).split(" ")).mapToInt(Integer::parseInt).toArray();
     }
-
-    private void solution() {
-        int blinkCount = 25;
-        int currentBlink = blinkCount;
-        StoneArrangement stones = readInput();
-        while (currentBlink > 0) {
-            stones = blink(stones);
-            System.out.printf("blink: %s, nodes: %s %n", (blinkCount - currentBlink + 1), stones.totalNodes);
-            currentBlink--;
-        }
-    }
-
-    private StoneArrangement blink(StoneArrangement stones) {
-        long totalNodes = 0;
-        Node pn = null;
-        Node head = stones.head;
-        Node cn = stones.head;
-
-        while (cn != null) {
-            totalNodes++;
-            Long cnv = cn.val;
-            if (cnv == 0) {
-                cn.val = 1L;
-            } else if (String.valueOf(cnv).length() % 2 == 0) {
-                totalNodes++;
-                List<Node> nodes = splitNode(cn, pn, head);
-                cn = nodes.get(1);
-                head = nodes.get(0);
-
-            } else {
-                cn.val = cnv * 2024;
-            }
-            pn = cn;
-            cn = cn.next;
-        }
-        return new StoneArrangement(head, stones.tail, totalNodes);
-    }
-
-    private List<Node> splitNode(Node n, Node pn, Node head) {
-        int l = String.valueOf(n.val).length();
-        Node n1 = new Node(String.valueOf(n.val).substring(0, l / 2));
-        Node n2 = new Node(String.valueOf(n.val).substring(l / 2, l));
-
-        n1.next = n2;
-        if (pn != null) {
-            pn.next = n1;
+    private List<Long> applyRule(long n) {
+        List<Long> r = new ArrayList<>(2);
+        if (n == 0) {
+            r.add(1L);
+        } else if (String.valueOf(n).length() % 2 == 0) {
+            String s = String.valueOf(n);
+            r.add(Long.valueOf(s.substring(0, s.length() / 2)));
+            r.add(Long.valueOf(s.substring(s.length() / 2)));
         } else {
-            head = n1;
+            r.add(2024 * n);
         }
-        if (n.next != null) {
-            n2.next = n.next;
-        }
-        return Arrays.asList(head, n2);
+        return r;
     }
 
+    private long blink(long n, int remainingBlink) {
+        if (remainingBlink == 0) {
+           return 1;
+        }
+        String key = String.format("%s-%s", n, remainingBlink);
+        if (!cache.containsKey(key)) {
+            List<Long> l = applyRule(n);
+            long count = 0;
+            for (Long i : l) {
+                String key1 = String.format("%s-%s", i, remainingBlink-1);
+                if(cache.containsKey(key1)){
+                   count = count+cache.get(key1);
+                }else{
+                  count = count + blink(i, remainingBlink-1);
+                }
+            }
+            cache.put(key, count);
+        }
+        return cache.getOrDefault(key, 0L);
+    }
+    private void solution(int blinkCount) {
+        cache = new HashMap<>();
+        int[] input = readInput();
+        long count = 0;
+        for (int i : input) {
+            count = count+ blink(i,blinkCount);
+        }
+        System.out.printf("No of stones after %s blinks: %s%n", blinkCount, count);
+    }
     public static void main(String[] args) {
         Day11 d = new Day11();
-        d.solution();
+        d.solution(25);//197157
+        d.solution(75);//234430066982597
     }
 }
